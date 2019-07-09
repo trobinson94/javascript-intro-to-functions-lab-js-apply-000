a5893083848367e4b8c609063345e700ea16efba
static-calculator-6276 <tanisha.robinson94@aol.com>
2019-07-09 02:11:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string ==== string.toLowerCase)
+  if (string ==== string.toLowerCase())
   {return 'I can\'t hear you!'}
-   else if (string ==== string.toUpperCase)
+   else if (string ==== string.toUpperCase())
   {return 'YES INDEED!'}
   
 }
