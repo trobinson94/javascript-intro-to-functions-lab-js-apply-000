586b10d5102b5cb8a779739477efefa759c3da49
static-calculator-6276 <tanisha.robinson94@aol.com>
2019-07-09 02:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   if (string ==== string.toLowerCase)
   return 'I can\'t hear you!'
   
-  if (string ==== string.toUpperCase)
+ else if (string ==== string.toUpperCase)
   return 'YES INDEED!'
   
 }
